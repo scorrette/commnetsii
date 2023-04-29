e973db318286763814116350ab8fd30b406f52e6
Jc2375 <80490009+Jc2375@users.noreply.github.com>
2023-04-28 21:53:39 -0400

    Update topoToGraph.py

--- topoToGraph.py
+++ topoToGraph.py
@@ -1,19 +1,17 @@
-#!/usr/bin/python3
 from test4 import NetworkTopo, run
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
 from mininet.log import setLogLevel, info
 from dijkstra import Graph
-def createGraph(v_count,vertices,net): #create graph based on number of vertices
+def createGraph(h_count,r_count,vertices,net): #create graph based on number of vertices
+    v_count = h_count+r_count #total node/vertice count
     g = Graph(v_count) #initialized graph
     links= []
+    dist = []
+    nodeHop_map=[]
     for each in net.links:
         links.append(each)
-    for each in net.hosts:
-        for link in links:
-            if each == link.intf1.node:
-                print("yes")
     for x in range(v_count):
         for y in range(v_count):
             for link in links:
@@ -23,22 +21,31 @@ def createGraph(v_count,vertices,net): #create graph based on number of vertices
                 elif x==y:
                     g.graph[x][y] = 0
     print(g.graph)
-    g.dijkstra(0)
+    dist =g.dijkstra(h_count)  #picks first router which we assume to be default RP as source, and returns distance list with corresponding vertice as index after dijkstra
+    for i in range(h_count):
+        if i==0:
+            continue
+        else:
+            nodeHop_map.append((net.hosts[i],dist[i]))
+    print(nodeHop_map)
 
-def run1():
+def run1(): ##This should be done in top file, but can call createGraph from Top file
     topo = NetworkTopo()
     net = Mininet(topo=topo, controller = None)
     router_count=0
-    router_list = []
+    host_count = 0
+    node_list = []
     router_list2=[]
     for each in net.hosts: ##can i use net.hosts?
-        #if each.name[0] =='r':  #confirm host is not a client but a router
-        router_count = router_count + 1;  
-        router_list.append(each)
-    for each in router_list:
+        if each.name[0] =='h':  #confirm host is a client 
+            host_count = host_count +1
+        else:
+            router_count = router_count + 1  
+        node_list.append(each)
+    for each in node_list:
         print(each)
     run(net)
-    createGraph(router_count,router_list,net)
+    createGraph(host_count , router_count,node_list,net)
 
 if __name__ == '__main__':
     setLogLevel('info')
