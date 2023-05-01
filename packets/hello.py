5c2c5fef7560ae94743c707d63a1a53b4d38f229
gui <guihori2000@hotmail.com>
2023-05-01 19:04:52 -0400

    use struct calcsize for other packets

--- packets/hello.py
+++ packets/hello.py
@@ -4,17 +4,16 @@ import struct
 # Hello Packet
 # Type 1
 PKT_TYPE = 1
-
+PACK_FORMAT = "BBBL"
 
 def create_packet(seq, ttl, src):
     """Create a new packet based on given id"""
     # pktType(1),  seq(1), ttl(1),  src(4)
-    header = struct.pack('BBBL', PKT_TYPE, seq, ttl, src)
+    header = struct.pack(PACK_FORMAT, PKT_TYPE, seq, ttl, src)
     return header
 
 
 def read_header(pkt):
-    header = pkt[0:32]
-    # pktFormat = "BBBL"
-    pkttype, seq, ttl, src = struct.unpack("BBBL", header)
+    header = pkt[0:struct.calcsize(PACK_FORMAT)]
+    pkttype, seq, ttl, src = struct.unpack(PACK_FORMAT, header)
     return pkttype, seq, ttl, src
--- packets/helloACK.py
+++ packets/helloACK.py
@@ -4,17 +4,17 @@ import struct
 # Hello ACK Packet
 # Type 2
 PKT_TYPE = 2
-
+PACK_FORMAT = "BBBLL"
 
 def create_packet(seq, ttl, src, dest):
     """Create a new packet based on given id"""
     # pktType(1),  seq(1), ttl(1), src(4), dest(4)
-    header = struct.pack('BBBLL', PKT_TYPE, seq, ttl, src, dest)
+    header = struct.pack(PACK_FORMAT, PKT_TYPE, seq, ttl, src, dest)
     return header
 
 
 def read_header(pkt):
-    header = pkt[0:32]
+    header = pkt[0:struct.calcsize(PACK_FORMAT)]
     # pktFormat = "BBBLL"
-    pkttype, seq, ttl, src, dest = struct.unpack("BBBLL", header)
+    pkttype, seq, ttl, src, dest = struct.unpack(PACK_FORMAT, header)
     return pkttype, seq, ttl, src, dest
