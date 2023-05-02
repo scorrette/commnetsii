c1419ce55e461daf4c6e7ee6a3ed48041f63a18c
Jc2375 <80490009+Jc2375@users.noreply.github.com>
2023-05-01 21:24:12 -0400

    Update topoToGraph.py
    updated to run statically.  **NOTE since its hardcoded, if you want to run other examples you must change "nodes_ex1 and links_ex1 in the getNodeHopMap function to nodes_ex2 or whatever examples number to match staticTables.py

--- topoToGraph.py
+++ topoToGraph.py
@@ -3,53 +3,59 @@ from mininet.net import Mininet
 from mininet.node import Node
 from mininet.log import setLogLevel, info
 from dijkstra import Graph
-
-
+from staticTables import nodes_ex1, links_ex1,nodes_ex2, links_ex2,nodes_ex3, links_ex3
 # create graph based on number of vertices
-def createGraph(h_count, r_count, net):
-    v_count = h_count + r_count  # total node/vertice count
-    g = Graph(v_count)  # initialized graph
+def createGraph(h_count, r_count,nodes,links):
+    v_count = h_count + r_count #total node/vertice count
+    g = Graph(v_count) #initialized graph
 
-    links = []
+    links_list= []
+    nodes_list = []
     dist = []
-    nodeHop_map = []
-    router_nodeHop = []
+    nodeHop_map=[]
+    router_nodeHop=[]
+    staticRP =""
 
-    for each in net.links:
-        links.append(each)
+    #find name of statiicRP which is assumed to be the router connected to h1
+    for each in links:
+        if link[0] == "h1":
+            staticRP = link[1]
+        elif link[1] == "h1":
+            staticRP = link[0]
+    for key in nodes:
+        nodes_list.append(key)   #generate list of node names from map
     for x in range(v_count):
         for y in range(v_count):
-            for link in links:
-                if (net.hosts[x] == link.intf1.node and net.hosts[y] == link.intf2.node) or (
-                        net.hosts[y] == link.intf1.node and net.hosts[x] == link.intf2.node):
+            for link in links:  #iterates over each tuple which represents a link
+                if (nodes_list[x] == links[0] and nodes_list[y] == link[1] or (nodes_list[y] == link[0] and nodes_list[x] == link[1]):
                     g.graph[x][y] = 1
                     g.graph[y][x] = 1
                 elif x == y:
                     g.graph[x][y] = 0
-
-    # picks first router which we assume to be default RP as source, and returns distance list with corresponding
-    # vertice as index after dijkstra
-
-    for j in range(r_count):  # find nodehop map for each router
-        dist = g.dijkstra(h_count + j)
-        router_nodeHop = []
+                    
+    
+            
+    # picks first router which we assume to be default RP as source, and returns distance list with corresponding vertice as index after dijkstra
+    
+    for j in range r_count:  #find nodehop map for each router
+        dist = g.dijkstra(h_count+j)
+        router_nodeHop=[]
         for i in range(h_count):
-            if i == 0:
+            if i == 0:   #skip h1 to static rp as it should always be 1
                 continue
             else:
-                router_nodeHop.append((net.hosts[i], dist[i]))
-        nodeHop_map.append(router_nodeHop)  # add map of router nodehops, to overall nodeHopmap
+                router_nodeHop.append((nodes_list[i], dist[i]))
+        nodeHop_map.append(router_nodeHop)  #add map of router nodehops, to overall nodeHopmap 
     return nodeHop_map
 
-
-def getNodeHopMap(net):
+def getNodeHopMap( ):
     rcount = 0
     hcount = 0
 
-    for h in net.hosts:
-        if h.name[0] == 'h':  # confirm host is a client
+    for nodeName in nodes_ex1:
+        if nodeName[0] =='h':  #confirm host is a client 
             hcount = hcount + 1
         else:
             rcount = rcount + 1
 
-    return createGraph(hcount, rcount, net)
+    return createGraph(hcount, rcount,nodes_ex1,links_ex1)
